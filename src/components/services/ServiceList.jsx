19480c78a371bd38b1bf0b734f331f06a434527e
Gosha Belenkiy <goshab@gmail.com>
2025-04-03 10:59:12 -0400

    Rewrite services and projects

--- src/components/services/ServiceList.jsx
+++ src/components/services/ServiceList.jsx
@@ -2,18 +2,14 @@ const ServiceList = [
     {
         id: 1,
         title: "Expert Implementation & Configuration",
-        // company: "Kurtosys",
-        // yearsActive: "January 2022 - Present",
         information: [
-            "Specialized deployment of IBM DataPower, API Connect, and Cloud Pak for Integration (CP4i)",
+            "Specialized deployment of IBM API Connect, Cloud Pak for Integration (CP4i), DataPower, and DataPower Operations Dashboard",
             "End-to-end professional services for seamless integration",
         ],
     },
     {
         id: 2,
         title: "Custom Solution Design",
-        // company: "Entrostat",
-        // yearsActive: "May 2017 - December 2021",
         information: [
             "Architecture and implementation of scalable, secure, high-performance integration systems",
             "Business-aligned solutions tailored to your specific requirements",
@@ -22,8 +18,6 @@ const ServiceList = [
     {
         id: 3,
         title: "Performance Optimization",
-        // company: "Empire State",
-        // yearsActive: "Feburary 2016 - April 2017",
         information: [
             "Advanced troubleshooting and system fine-tuning",
             "Proactive health checks and continuous improvement strategies",
@@ -32,8 +26,6 @@ const ServiceList = [
     {
         id: 4,
         title: "Knowledge Transfer & Training",
-        // company: "Empire State",
-        // yearsActive: "Feburary 2016 - April 2017",
         information: [
             "Comprehensive training programs for internal teams",
             "Documentation and best practice guidance for long-term success",
@@ -42,8 +34,6 @@ const ServiceList = [
     {
         id: 5,
         title: "DevOps & Automation",
-        // company: "Empire State",
-        // yearsActive: "Feburary 2016 - April 2017",
         information: [
             "Custom automation solutions to streamline workflows",
             "CI/CD pipeline implementation for efficient deployments",
@@ -52,8 +42,6 @@ const ServiceList = [
     {
         id: 6,
         title: "Strategic Consulting",
-        // company: "Empire State",
-        // yearsActive: "Feburary 2016 - April 2017",
         information: [
             "Requirements analysis and solution scoping",
             "Stakeholder collaboration to ensure business alignment",
